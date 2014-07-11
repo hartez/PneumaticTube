9d6f4732cea105211aa1e58623d1e339ec9a258b
E.Z. Hart <hartez@gmail.com>
2014-07-11 13:14:08 -0600

    Update assemblyinfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PneumaticTube")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Command line Dropbox uploader for Windows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Traceur LLC")]
 [assembly: AssemblyProduct("PneumaticTube")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © Traceur LLC 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
