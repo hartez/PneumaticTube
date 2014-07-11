7a5dba06d6760530e25348658ab6ecb083e1299f
E.Z. Hart <hartez@gmail.com>
2014-07-11 12:39:49 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using CommandLine;
+using DropNet;
+using DropNet.Models;
+using PneumaticTube.Properties;
+
+namespace PneumaticTube
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var options = new UploadOptions();
+
+            if(!Parser.Default.ParseArguments(args, options))
+            {
+                Console.WriteLine(options.GetUsage());
+                return;
+            }
+
+            var client = new DropNetClient(ConfigurationManager.AppSettings["API_KEY"],
+                ConfigurationManager.AppSettings["API_SECRET"]);
+
+            // See if we have a token already
+            if(String.IsNullOrEmpty(Settings.Default.USER_TOKEN) || String.IsNullOrEmpty(Settings.Default.USER_SECRET))
+            {
+                Console.WriteLine(
+                    "You'll need to authorize this account with PneumaticTube; a browser window will now open asking you to log into dropbox and allow the app. When you've done that, hit Enter.");
+
+                // Pop open the authorization page in the default browser
+                var url = client.GetTokenAndBuildUrl();
+                Process.Start(url);
+
+                Console.ReadLine();
+
+                var accessToken = client.GetAccessToken();
+
+                // Save the token and secret 
+                Settings.Default.USER_SECRET = accessToken.Secret;
+                Settings.Default.USER_TOKEN = accessToken.Token;
+                Settings.Default.Save();
+            }
+
+            client.UserLogin = new UserLogin
+            {
+                Token = Settings.Default.USER_TOKEN,
+                Secret = Settings.Default.USER_SECRET
+            };
+
+            var source = Path.GetFullPath(options.LocalPath);
+            var filename = Path.GetFileName(source);
+
+            if(!File.Exists(source))
+            {
+                Console.WriteLine("Source file does not exist.");
+                return;
+            }
+
+            Console.WriteLine("Uploading {0} to {1}", filename, options.DropboxPath);
+
+            using(var fs = new FileStream(source, FileMode.Open, FileAccess.Read))
+            {
+                var uploaded = client.UploadFile(options.DropboxPath, filename, fs);
+
+                Console.WriteLine("Whoosh...");
+                Console.WriteLine("Uploaded {0} to {1}; Revision {2}", uploaded.Name, uploaded.Path, uploaded.Revision);
+            }
+        }
+    }
+}
---
+++ UploadOptions.cs
@@ -0,0 +1,27 @@
+using CommandLine;
+using CommandLine.Text;
+
+namespace PneumaticTube
+{
+    internal class UploadOptions
+    {
+        [Option('f', "file", Required = true, HelpText = "The location of the file to upload")]
+        public string LocalPath { get; set; }
+
+        [Option('p', "path", Required = true, HelpText = "The destination path in Dropbox")]
+        public string DropboxPath { get; set; }
+
+        public string GetUsage()
+        {
+            var help = new HelpText
+            {
+                AddDashesToOption = true
+            };
+
+            help.AddPreOptionsLine("pneumatictube -f <file> -p <path>");
+            help.AddOptions(this);
+
+            return help.ToString();
+        }
+    }
+}
