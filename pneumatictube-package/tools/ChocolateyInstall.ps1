1a84a113e4f24b269162990b375f58a9a622dff1
E.Z. Hart <hartez@gmail.com>
2014-07-14 13:38:43 -0600

    Update install root for package

--- pneumatictube-package/tools/ChocolateyInstall.ps1
+++ pneumatictube-package/tools/ChocolateyInstall.ps1
@@ -2,8 +2,7 @@ $packageName = 'PneumaticTube.portable'
 $url = 'https://github.com/hartez/PneumaticTube/releases/download/1.0.0/PneumaticTube.zip' 
 
 try { 
-  $binRoot = "$env:systemdrive\tools"
-  if($env:chocolatey_bin_root -ne $null -and $env:chocolatey_bin_root -notlike '*:\*'){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $binRoot = Get-BinRoot
   $installDir = Join-Path $binRoot "$packageName"
   Write-Host "Adding `'$installDir`' to the path and the current shell path"
   Install-ChocolateyPath "$installDir"
