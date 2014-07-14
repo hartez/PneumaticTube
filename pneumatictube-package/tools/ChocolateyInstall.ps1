8fcee52a00b6a9352bae1cee45e09c9bdb54a101
E.Z. Hart <hartez@gmail.com>
2014-07-14 12:22:50 -0600

    Updating package to portable from zip

--- pneumatictube-package/tools/ChocolateyInstall.ps1
+++ pneumatictube-package/tools/ChocolateyInstall.ps1
@@ -1,7 +1,18 @@
-$packageName = 'PneumaticTube' 
-$installerType = 'MSI' 
-$url = 'https://github.com/hartez/PneumaticTube/releases/download/1.0.0/PneumaticTube.Setup.msi' 
-$silentArgs = '/quiet' 
-$validExitCodes = @(0) 
+$packageName = 'PneumaticTube.portable' 
+$url = 'https://github.com/hartez/PneumaticTube/releases/download/1.0.0/PneumaticTube.zip' 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+try { 
+  $binRoot = "$env:systemdrive\tools"
+  if($env:chocolatey_bin_root -ne $null -and $env:chocolatey_bin_root -notlike '*:\*'){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot "$packageName"
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  Install-ChocolateyPath "$installDir"
+  $env:Path = "$($env:Path);$installDir"
+ 
+  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
+ 
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
