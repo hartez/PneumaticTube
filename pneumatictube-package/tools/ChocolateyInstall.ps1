9e3a628ac1d26d2228f975e83db602d0fa5a3495
E.Z. Hart <hartez@gmail.com>
2014-10-27 18:27:29 -0600

    Fix package installer to use default directory

--- pneumatictube-package/tools/ChocolateyInstall.ps1
+++ pneumatictube-package/tools/ChocolateyInstall.ps1
@@ -2,14 +2,8 @@ $packageName = 'PneumaticTube.portable'
 $url = 'https://github.com/hartez/PneumaticTube/releases/download/1.0.2/PneumaticTube.zip' 
 
 try { 
-  $binRoot = Get-BinRoot
-  $installDir = Join-Path $binRoot "$packageName"
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  Install-ChocolateyPath "$installDir"
-  $env:Path = "$($env:Path);$installDir"
- 
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
- 
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
