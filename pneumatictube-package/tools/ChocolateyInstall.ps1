1d2f3ec40740361720caac7b8ab526daecb99719
E.Z. Hart <hartez@gmail.com>
2015-11-15 12:28:05 -0700

    Updated chocolatey package

--- pneumatictube-package/tools/ChocolateyInstall.ps1
+++ pneumatictube-package/tools/ChocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'PneumaticTube.portable' 
-$url = 'https://github.com/hartez/PneumaticTube/releases/download/1.2.0/PneumaticTube.zip' 
+$url = 'https://github.com/hartez/PneumaticTube/releases/download/1.2/PneumaticTube.zip' 
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" 
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" -md5 "ABAAA38826D87D63AC41CA0DDE104489"
