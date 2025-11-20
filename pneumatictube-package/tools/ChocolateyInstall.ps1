622e71f694255fad9705a3c86def462d26087979
E.Z. Hart <hartez@gmail.com>
2025-11-19 18:21:39 -0700

    Prepping for 1.8 release

--- pneumatictube-package/tools/ChocolateyInstall.ps1
+++ pneumatictube-package/tools/ChocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'PneumaticTube.portable' 
-$url = 'https://github.com/hartez/PneumaticTube/releases/download/v1.7/PneumaticTube.zip' 
+$url = 'https://github.com/hartez/PneumaticTube/releases/download/v1.8/PneumaticTube.zip' 
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" -Checksum "E822557FA42175A4A580BDF8FE02B61649BF1BD97EC54A78E2CC130F773E70DA" -ChecksumType sha256
