d25ec75946a93abcd7959fad110d66d0c9500dc7
E.Z. Hart <hartez@gmail.com>
2025-11-22 17:23:55 -0700

    Update checksum and logo location

--- pneumatictube-package/tools/ChocolateyInstall.ps1
+++ pneumatictube-package/tools/ChocolateyInstall.ps1
@@ -2,4 +2,4 @@ $packageName = 'PneumaticTube.portable'
 $url = 'https://github.com/hartez/PneumaticTube/releases/download/v1.8/PneumaticTube.zip' 
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" -Checksum "E822557FA42175A4A580BDF8FE02B61649BF1BD97EC54A78E2CC130F773E70DA" -ChecksumType sha256
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" -Checksum "1405D4E7B18AD3E9143A91930778288C0582DEEC7D244A357309A62274D2ECE2" -ChecksumType sha256
